423bdde6e6557a934ddc06ea0932e0b6fb4f71bf
SimBoo <thisiswoa@163.com>
2014-11-26 17:31:18 +0800

    基本版1.0

--- add_dots_automatic.js
+++ add_dots_automatic.js
@@ -1,9 +1,20 @@
-if ($) {
-    console.log("haha");
-}
+function addDotBeforeEachList($list_parent, ls_item_tag) {
+    var $item = $list_parent.children(ls_item_tag);
+    /* 设置 position relative*/
+    $item.css('position', 'relative');
+    /* 添加特定类名的点到每一个列表项元素*/
+    $item.each(function(index, ele) {
+    	$(ele).append("<b class='dots-before-li'></b>").hover(function() {
+    		/* Stuff to do when the mouse enters the element */
+    		$(this).find(".dots-before-li").addClass("dots-before-li-active");
+    	}, function() {
+    		/* Stuff to do when the mouse leaves the element */
+    		$(this).find(".dots-before-li").removeClass("dots-before-li-active");
+    	});
+    });
+};
 
 $(function() {
-	var $list_parent=$(".news");
-	//console.log("");
-	console.log("i do not know");
+    /* run the function*/
+    addDotBeforeEachList($(".news"), 'p');
 });
